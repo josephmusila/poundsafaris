7e8908c291a0d3b308bb2d893eafd663776b245b
josephmusila <josemusila03@gmail.com>
2023-11-11 14:10:17 +0300

    fixed overflow text in video section

--- poundvest/settings.py
+++ poundvest/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-sj-_jy6^^p2(dzreim%p)pw&^j3*e=ydmq1s8kt%ag+q0d4h40
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
