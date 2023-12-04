abf9fbe111509318eeb2c3bdc90792bb6095842a
joseph <josemusila03@gmail.com>
2023-12-04 17:03:11 +0300

    fixed bugs with translation package

--- poundvest/settings.py
+++ poundvest/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'google_translate',
+   
     'base',
     # 'django_pesapal',
 ]
